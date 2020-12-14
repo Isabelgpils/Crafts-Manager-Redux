f35563e0f555c9936dee959c407ef6f174a278ff
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2020-12-13 19:56:05 -0600

    Updates

---
+++ precache-manifest.637299abb494d9a4ff032abfce87ac2c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacdc43b5e3d9307e63c9756dafa9890",
+    "url": "/craft-manager/index.html"
+  },
+  {
+    "revision": "7acdec6cdf2e73c8603c",
+    "url": "/craft-manager/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "c2b953427fc7fd251461",
+    "url": "/craft-manager/static/css/main.211f9649.chunk.css"
+  },
+  {
+    "revision": "7acdec6cdf2e73c8603c",
+    "url": "/craft-manager/static/js/2.979c0469.chunk.js"
+  },
+  {
+    "revision": "38ea18c07d3e5b676b323ab6278c9aca",
+    "url": "/craft-manager/static/js/2.979c0469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b953427fc7fd251461",
+    "url": "/craft-manager/static/js/main.308f8b49.chunk.js"
+  },
+  {
+    "revision": "7e93b26dc1ffa9eb9ff2",
+    "url": "/craft-manager/static/js/runtime-main.80c6c719.js"
+  },
+  {
+    "revision": "6d1f68619dd133fcb3b986b413b234b2",
+    "url": "/craft-manager/static/media/PeacockTile.6d1f6861.jpg"
+  },
+  {
+    "revision": "d3f92077a0d8659f1c04a589294a2111",
+    "url": "/craft-manager/static/media/buttercup.d3f92077.jpg"
+  },
+  {
+    "revision": "10eec513b5e2d679d4067472d542b429",
+    "url": "/craft-manager/static/media/cottageFairy.10eec513.jpg"
+  },
+  {
+    "revision": "fac7930b3a235d53a6bbff1e3012a7ac",
+    "url": "/craft-manager/static/media/dragonfly3.fac7930b.jpg"
+  },
+  {
+    "revision": "739721e159d8109780fa240a8239be77",
+    "url": "/craft-manager/static/media/empty.739721e1.jpg"
+  },
+  {
+    "revision": "e0004624c81d8364f60946e70eb52b90",
+    "url": "/craft-manager/static/media/geranium.e0004624.jpg"
+  },
+  {
+    "revision": "1e25a45cc4531e7a05009abe5e545899",
+    "url": "/craft-manager/static/media/gothicArch.1e25a45c.jpg"
+  },
+  {
+    "revision": "ec7e8c0c5241c6f3ccf308ce39573ee4",
+    "url": "/craft-manager/static/media/granada2.ec7e8c0c.jpg"
+  },
+  {
+    "revision": "23f814b291abd8343f28c0fe5b1c9eac",
+    "url": "/craft-manager/static/media/gypsy2.23f814b2.jpg"
+  },
+  {
+    "revision": "f729ce9eb397c4cee60f1c2e1d89335c",
+    "url": "/craft-manager/static/media/hanging1.f729ce9e.jpg"
+  },
+  {
+    "revision": "69559ee1eb03c9cb22e60017b1640d47",
+    "url": "/craft-manager/static/media/herbularus.69559ee1.jpg"
+  },
+  {
+    "revision": "eb142f178b9571041dffe318891066c0",
+    "url": "/craft-manager/static/media/hortager.eb142f17.jpg"
+  },
+  {
+    "revision": "65e4d5bddadf10f307b5cfaa1d29d58e",
+    "url": "/craft-manager/static/media/hummingbird.65e4d5bd.jpg"
+  },
+  {
+    "revision": "b34cf33387ccf8ec3b15e075bcbfd29e",
+    "url": "/craft-manager/static/media/hydrangea.b34cf333.jpg"
+  },
+  {
+    "revision": "cbb74c84835a9660d581e9b78faae8f9",
+    "url": "/craft-manager/static/media/miniTuscany2.cbb74c84.jpg"
+  },
+  {
+    "revision": "f3e5d54919dcba6fbe654dbaff297111",
+    "url": "/craft-manager/static/media/mossGarden2.f3e5d549.jpg"
+  },
+  {
+    "revision": "28106ae3f65f36cc251314708b68f691",
+    "url": "/craft-manager/static/media/persianIris.28106ae3.jpg"
+  },
+  {
+    "revision": "2c0166a4082dcabc26910165a1c8a185",
+    "url": "/craft-manager/static/media/portager2.2c0166a4.jpg"
+  },
+  {
+    "revision": "7c774ec09ed981cfdd40eb4556ad50e2",
+    "url": "/craft-manager/static/media/sapphireFairy.7c774ec0.jpg"
+  },
+  {
+    "revision": "f235897239f394cb7a4adea426d4f197",
+    "url": "/craft-manager/static/media/tapestry.f2358972.jpg"
+  },
+  {
+    "revision": "6d0fe5ece2cd3dbf4a688e97aa3fa1de",
+    "url": "/craft-manager/static/media/vinery.6d0fe5ec.jpg"
+  },
+  {
+    "revision": "8e80bf03a0add67feb9cec4a8daf7ac2",
+    "url": "/craft-manager/static/media/waterGarden.8e80bf03.jpg"
+  }
+]);
