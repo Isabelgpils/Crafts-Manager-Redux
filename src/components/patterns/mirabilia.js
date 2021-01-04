2c3d9851943817199d6bdd636ab5a594a7683fe2
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-04 12:46:15 -0600

    added isFinished option

--- src/components/patterns/mirabilia.js
+++ src/components/patterns/mirabilia.js
@@ -5,10 +5,10 @@ import empty from './images/empty.jpg';
 import unicorn from './images/unicorn2.png';
 
 export const mirabilia = [
-	{ number: 137, size: "369 x 369", image: empty, thumbnail: "https://www.123stitch.com/pictures/11-1668.jpg", brand: 'Mirabilia' },
-	{ number: 137, size: "369 x 369", image: unicorn, name: 'Unicorn', brand: 'other' },
-	{ number: 198, size: "98 x 127", image: geranium, name: 'Geranium', brand: 'Nora Corbett', thumbnail: "https://www.123stitch.com/pictures/NC198.jpg" },
-	{ number: 111, size: "", image: empty, name: 'Sleeping Princess', brand: 'Mirabilia', thumbnail: "https://www.123stitch.com/pictures/12-2843.jpg" },
-	{ number: "MD 63", size: "210 x 271 ", image: empty, name: 'CottageFairy', thumbnail: cottageFairy, brand: 'Mirabilia' },
-	{ number: "MD 63", size: "210 x 271 ", image: gypsy, name: 'Gypsyy', thumbnail: empty, brand: 'Mirabilia' }
+	{ number: 137, size: "369 x 369", image: empty, thumbnail: "https://www.123stitch.com/pictures/11-1668.jpg", brand: 'Mirabilia', isFinished: false },
+	{ number: 137, size: "369 x 369", image: unicorn, name: 'Unicorn', brand: 'other', isFinished: false },
+	{ number: 198, size: "98 x 127", image: geranium, name: 'Geranium', brand: 'Nora Corbett', thumbnail: "https://www.123stitch.com/pictures/NC198.jpg", isFinished: false },
+	{ number: 111, size: "", image: empty, name: 'Sleeping Princess', brand: 'Mirabilia', thumbnail: "https://www.123stitch.com/pictures/12-2843.jpg", isFinished: false },
+	{ number: "MD 63", size: "210 x 271 ", image: empty, name: 'CottageFairy', thumbnail: cottageFairy, brand: 'Mirabilia', isFinished: false },
+	{ number: "MD 63", size: "210 x 271 ", image: gypsy, name: 'Gypsyy', thumbnail: empty, brand: 'Mirabilia', isFinished: false }
 ];
