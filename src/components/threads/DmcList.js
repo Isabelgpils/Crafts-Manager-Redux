5d0a7e754fb8d55932e72d79becfdebbdc90796a
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-10 12:34:08 -0600

    remove old commented code

--- src/components/threads/DmcList.js
+++ src/components/threads/DmcList.js
@@ -37,28 +37,5 @@ const DmcList = props => {
     </Fragment>
   );
 };
-{
-  /* <Grid container direction="column">
-        <div className="row">
-          <div className="col s12">
-            <h5 style={{ marginTop: "5%" }}>DMC Cotton Thread</h5>
-          </div>
-        </div>
-        <Grid item>
-          <Grid item container style={{ marginLeft: "25, 50px" }} />
-          <Grid item xs={false} sm={2} />
-          {/* on xsmall screens=no gutter  */
-}
-//           <Grid item xs={12}>
-//             <Grid item container>
-//               {allThreads}
-//             </Grid>
-//           </Grid>
-//           <Grid item xs={false} sm={2} />
-//         </Grid>
-//       </Grid> */}
-//     </Fragment>
-//   );
-// };
 
 export default DmcList;
