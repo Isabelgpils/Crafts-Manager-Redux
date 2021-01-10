b1390fa75d2b4da7e33d0d395a9c18cc31706489
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-10 12:31:51 -0600

    removed old commented code

--- src/components/threads/DinkyList.js
+++ src/components/threads/DinkyList.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import ThreadItem from "./ThreadItem";
 import { dinky } from "./dinky";
 import DinkyItem from "./DinkyItem";
 
