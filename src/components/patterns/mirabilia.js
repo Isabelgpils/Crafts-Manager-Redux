690806b6f1c6eb91985ac8b1b8a4ecbff7a9a2e3
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-14 13:15:11 -0600

    correct mispelled image name

--- src/components/patterns/mirabilia.js
+++ src/components/patterns/mirabilia.js
@@ -10,5 +10,5 @@ export const mirabilia = [
 	{ number: 198, size: "98 x 127", image: geranium, name: 'Geranium', brand: 'Nora Corbett', thumbnail: "https://www.123stitch.com/pictures/NC198.jpg", isFinished: false },
 	{ number: 111, size: "", image: empty, name: 'Sleeping Princess', brand: 'Mirabilia', thumbnail: "https://www.123stitch.com/pictures/12-2843.jpg", isFinished: false },
 	{ number: "MD 63", size: "210 x 271 ", image: empty, name: 'CottageFairy', thumbnail: cottageFairy, brand: 'Mirabilia', isFinished: false },
-	{ number: "MD 63", size: "210 x 271 ", image: gypsy, name: 'Gypsyy', thumbnail: empty, brand: 'Mirabilia', isFinished: false }
+	{ number: "MD 63", size: "210 x 271 ", image: gypsy, name: 'Gypsy', thumbnail: empty, brand: 'Mirabilia', isFinished: false }
 ];
