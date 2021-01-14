4ba37fcee533c362a721d4ebe885f0951e3fba56
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-14 13:36:30 -0600

    file containing array of beads with embedded array of patterns

---
+++ src/components/beads/beadsMain.js
@@ -0,0 +1,200 @@
+export const beads = [
+
+    { number: "0010", patterns: ["Dragonfly Tile", "Rainforest", "Watergarden"] },
+    { number: "0027", patterns: ["Arabian", "Dragonfly Tile", "Mini Mandelas", "Peacock Tile", "Fairy Flora"] },
+    { number: "0031", patterns: ["Consolation 2015", "Sparkling Peacock", "Tinctorium"] },
+    { number: "0047", patterns: ["Hummingbird Tile", "Persian Iris", "Rainforest"] },
+    { number: "0051", patterns: ["Arabian", "Hanging Gardens", "Fairy Flora", "Herbularus", "Persian Iris", "Tinctorium"] },
+    { number: "0057", patterns: ["Dragonfly Tile", "French Kitchen Garden", "Rainforest"] },
+    { number: "0058", patterns: ["Seville", "Fairy Flora"] },
+    { number: "0060", patterns: ["Consolation 2015", "French Kitchen Garden"] },
+    { number: "0078", patterns: ["Fairy Flora", "Hanging Gardens", "Herbularus", "Mini Mandelas"] },
+    { number: '0079', patterns: ['Little Tuscany', "Persian Iris"] },
+    { number: "0112", patterns: ["Consolation 2015", "Rainforest", "Tinctorium"] },
+    { number: "0117", patterns: ["Arabian", "Fairy Flora", "Persian Iris"] },
+    { number: "0124", patterns: ["Peacock Tile", "Persian Iris"] },
+    { number: '0125', patterns: ["Herbularus", "Persian Iris", "Rainforest"] },
+    { number: '0174', patterns: ["Herbularus", "Sparkling Peacock"] },
+    { number: '0276', patterns: ["Consolation 2015", "Rainforest", "Sparkling Peacock", "Tinctorium"] },
+    { number: "0331", patterns: ["Persian Iris", "Moss Garden"] },
+    { number: '0373', patterns: ["Moss Garden", "Water Garden"] },
+    { number: "0458", patterns: ["Hummingbird Tile", "Moss Garden", "Peacock Tile"] },
+    { number: "0459", patterns: ["Dragonfly Tile", "Hummingbird Tile"] },
+    {
+        number: "0501", patterns: ["Arabian", "Dragonfly Tile", "French Kitchen Garden",
+            "Hanging Gardens", "Hummingbird Tile", "Mini Mandelas",
+            "Sparkling Peacock", "Water Garden"]
+    },
+    { number: '0505', patterns: ['Autumn Hydrangea', "Herbularus"] },
+    { number: "0508", patterns: ["Fairy Flora", "Peacock Tile"] },
+    { number: "0622", patterns: ["Mini Mandelas", "Watergarden"] },
+    { number: "0624", patterns: ["Arabian", "Fairy Flora", "Moss Garden"] },
+    { number: '0688', patterns: ['Hummingbird Tile', "Rainforest", "Watergarden"] },
+    //Arabian
+    { number: "0250", patterns: "Arabian" },
+    { number: "0624", patterns: "Arabian" },
+    { number: "0871", patterns: "Arabian" },
+    { number: "0284", patterns: "Arabian" },
+    { number: "1805", patterns: "Arabian" },
+
+    //Consololation
+    { number: "0274", patterns: "Consolation 2015" },
+    { number: "0629", patterns: "Consolation 2015" },
+    { number: "0277", patterns: "Consolation 2015" },
+
+    //Dragonfly Tile
+    { number: "0052", patterns: "Dragonfly Tile" },
+    { number: "0084", patterns: "Dragonfly Tile" },
+    { number: "0166", patterns: "Dragonfly Tile" },
+    { number: "0285", patterns: "Dragonfly Tile" },
+    { number: "0747", patterns: "Dragonfly Tile" },
+    { number: "0862", patterns: "Dragonfly Tile" },
+
+    //Fairy Flora
+    { number: "0114", patterns: "Fairy Flora" },
+    { number: "1343", patterns: "Fairy Flora" },
+    { number: "0411", patterns: "Fairy Flora" },
+    { number: "1455", patterns: "Fairy Flora" },
+
+
+    //French Kitchen Garden
+    { number: '0747', patterns: 'French Kitchen Garden' },
+    { number: "0779", patterns: "French Kitchen Garden" },
+    { number: '0832', patterns: 'French Kitchen Garden' },
+    { number: '0922', patterns: 'French Kitchen Garden' },
+    { number: "1790", patterns: "French Kitchen Garden" },
+    { number: "1812", patterns: "French Kitchen Garden" },
+    { number: "1816", patterns: "French Kitchen Garden" },
+
+    //Hanging Gardens
+    { number: '0918', patterns: 'Hanging Gardens' },
+    { number: "1741", patterns: "Hanging Gardens" },
+    { number: "1788", patterns: "Hanging Gardens" },
+
+    //Herbularus
+    { number: '0864', patterns: 'Herbularus' },
+    { number: "0135", patterns: 'Herbularus' },
+    { number: '0858', patterns: 'Herbularus' },
+    { number: "0296", patterns: "Herbularus" },
+    { number: "0881", patterns: "Herbularus" },
+
+
+    //Hummingbird Tile
+
+    { number: "0705", patterns: "Hummingbird Tile" },
+    { number: '0670', patterns: 'Hummingbird Tile' },
+    { number: "0413", patterns: "Hummingbird Tile" },
+
+    { number: "0879", patterns: "Hummingbird Tile" },
+    { number: "0864", patterns: "Hummingbird Tile" },
+    { number: "0985", patterns: "Hummingbird Tile" },
+    { number: "0917", patterns: "Hummingbird Tile" },
+
+
+    //Hydrangea
+    { number: "0356", patterns: "Autumn Hydrangea" },
+    { number: "0372", patterns: "Autumn Hydrangea" },
+
+    { number: "0656", patterns: "Autumn Hydrangea" },
+    { number: "0832", patterns: "Autumn Hydrangea" },
+    { number: '1455', patterns: 'Autumn Hydrangea' },
+    { number: '1812', patterns: 'Autumn Hydrangea' },
+    { number: '1816', patterns: 'Autumn Hydrangea' },
+
+    //Little Tuscany
+
+    { number: "0859", patterns: "Little Tuscany" },
+    { number: "1763", patterns: "Little Tuscany" },
+    { number: "1783", patterns: "Little Tuscany" },
+    { number: "1833", patterns: "Little Tuscany" },
+
+    //Mini Mandelas
+    { number: "0111", patterns: "Mini Mandelas" },
+    { number: "0173", patterns: "Mini Mandelas" },
+
+    //Moss Garden
+    { number: "0053", patterns: "Moss Garden" },
+    { number: '0906', patterns: 'Moss Garden' },
+    { number: "1109", patterns: "Moss Garden" },
+    { number: "1845", patterns: "Moss Garden" },
+    { number: "0165", patterns: "Moss Garden" },
+
+    //Peacock Tile
+    { number: "0607", patterns: "Peacock Tile" },
+    { number: "0609", patterns: "Peacock Tile" },
+    { number: "0610", patterns: "Peacock Tile" },
+    { number: '1005', patterns: 'Peacock Tile' },
+    { number: "1006", patterns: "Peacock Tile" },
+
+    //Persian Iris
+
+    { number: "0374", patterns: "Persian Iris" },
+    { number: "0608", patterns: "Persian Iris" },
+    { number: "0694", patterns: "Persian Iris" },
+    { number: '0230', patterns: 'Persian Iris' },
+    { number: "0080", patterns: "Persian Iris" },
+    { number: "0906", patterns: "Persian Iris" },
+
+    //Rainforest
+    { number: "0275", patterns: "Rainforest" },
+    { number: "0410", patterns: "Rainforest" },
+    { number: "0693", patterns: "Rainforest" },
+    { number: '0747', patterns: 'Rainforest' },
+    { number: "0755", patterns: "Rainforest" },
+    { number: "0795", patterns: "Rainforest" },
+    { number: "1249", patterns: "Rainforest" },
+    { number: "1505", patterns: "Rainforest" },
+    { number: "1578", patterns: "Rainforest" },
+    { number: "1731", patterns: "Rainforest" },
+    { number: "0077", patterns: "Rainforest" },
+    { number: "0029", patterns: "Rainforest" },
+    { number: "0297", patterns: "Rainforest" },
+    { number: "0777", patterns: "Rainforest" },
+    { number: "0856", patterns: "Rainforest" },
+    { number: "1585", patterns: "Rainforest" },
+
+    //Seville
+    { number: "0034", patterns: "Seville" },
+    { number: "0035", patterns: "Seville" },
+    { number: "0177", patterns: "Seville" },
+    { number: "1710", patterns: "Seville" },
+    { number: '1741', patterns: 'Seville' },
+    { number: "1766", patterns: "Seville" },
+    { number: '1812', patterns: 'Seville' },
+    { number: "1764", patterns: "Seville" },
+
+    //Sparkling Peacock
+    { number: "0918", patterns: "Sparkling Peacock" },
+    { number: '0051', patterns: 'Sparkling Peacock' },
+    { number: "1769", patterns: "Sparkling Peacock" },
+    { number: "0688", patterns: "Sparkling Peacock" },
+    { number: "0152", patterns: "Sparkling Peacock" },
+
+
+    //Tinctorium
+    { number: "0109", patterns: "Tinctorium" },
+    { number: "0237", patterns: "Tinctorium" },
+    { number: "0626", patterns: "Tinctorium" },
+    { number: '0694', patterns: 'Tinctorium' },
+    { number: "0861", patterns: "Tinctorium" },
+
+    //Vineyard
+    { number: "0170", patterns: "Vineyard" },
+    { number: "0233", patterns: "Vineyard" },
+    { number: "0238", patterns: "Vineyard" },
+    { number: "0858", patterns: "Vineyard" },
+    { number: "0880", patterns: "Vineyard" },
+    { number: "0922", patterns: "Vineyard" },
+    { number: "1005", patterns: "Vineyard" },
+    { number: "1185", patterns: "Vineyard" },
+    { number: "1247", patterns: "Vineyard" },
+
+    //Watergarden
+    { number: "0324", patterns: "Watergarden" },
+    { number: "0352", patterns: "Watergarden" },
+    { number: "0354", patterns: "Watergarden" },
+    { number: "0355", patterns: "Watergarden" },
+    { number: "0415", patterns: "Watergarden" },
+    { number: "0877", patterns: "Watergarden" }
+
+];
