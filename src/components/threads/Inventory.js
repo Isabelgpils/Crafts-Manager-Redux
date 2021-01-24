35547a62fd8361f8e79575189073b4d60dc72444
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-24 17:01:01 -0600

    component that allows user to determine if item is needed or in stash

---
+++ src/components/threads/Inventory.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const Inventory = () => {
+
+
+    const [inventory, setInventory] = useState('');
+    return (
+        <div>
+            <Typography style={{ textAlign: "center", color: "rgb(56,64,88)", color: "white", background: "darkGrey" }} variant="h6" component="h6">
+                {inventory}
+            </Typography >
+            <Button size="medium" onClick={() => setInventory('In Stash')} style={{ marginLeft: "25px" }} >
+                In Stash
+				</Button>
+            <Button size="medium" onClick={() => setInventory("Need")} style={{ marginLeft: "85px" }} >
+                Need
+				</Button>
+        </div>
+    )
+}
+export default Inventory;
