fa68b596357b3663e172d95aec92f8e9688e1df6
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2021-01-24 17:25:34 -0600

    remove Material-UI button

--- src/components/threads/ThreadItem.js
+++ src/components/threads/ThreadItem.js
@@ -4,9 +4,10 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Inventory from '../Inventory'
+
 
 const ThreadItem = props => {
 
@@ -73,12 +74,7 @@ const ThreadItem = props => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="medium" style={{ marginLeft: "5%" }}>
-          In Stash
-        </Button>
-        <Button size="medium" style={{ marginLeft: "30%" }}>
-          Need
-        </Button>
+          <Inventory />
       </CardActions>
     </Card>
   );
